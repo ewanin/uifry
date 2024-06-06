6cccb9f77751f7120a9aa2fe8d1e8e9d57cf2497
Sahil Ninawe <ninawesahil12@gmail.com>
2024-06-06 12:21:34 +0530

    Updated Footer & data.json

--- src/app/components/Footer/FooterOne.jsx
+++ src/app/components/Footer/FooterOne.jsx
@@ -20,7 +20,7 @@ const FooterOne = () => {
                             ) : (
                                 // Render object item with icon
                                 <div className='flex items-center sm:gap-2 gap-1'>
-                                    <img src={item.icon} alt="Logo" />
+                                    {item.icon && <img src={item.icon} alt="Logo" />}
                                     <h3 className='text-[16px] leading-[26px]'>{item.text}</h3>
                                 </div>
                             )}
@@ -28,9 +28,11 @@ const FooterOne = () => {
                     ))}
                     {/* Render newsletter input and button */}
                     {section.title === 'Newsletter' && (
-                        <div className='relative font-clashMedium sm:w-[340px] h-[76px] flex sm:flex-row flex-col sm:gap-0 gap-4 sm:items-center justify-between'>
-                            <input type="email" placeholder={section.items[0].inputPlaceholder} className='p-[10px] w-full h-full shadow-custom' />
-                            <button className='bg-black hover:bg-pink text-white hover:text-black text-[16px] leading-[26px] rounded px-10 py-4 sm:absolute left-1/2'>{section.items[0].buttonText}</button>
+                        <div>
+                            <div className='relative font-clashMedium sm:w-[340px] h-[76px] flex sm:flex-row flex-col sm:gap-0 gap-4 sm:items-center justify-between'>
+                                <input type="email" placeholder={section.items[0].inputPlaceholder} className='p-[10px] w-full h-full shadow-custom' />
+                                <button className='bg-black hover:bg-pink text-white hover:text-black text-[16px] leading-[26px] rounded px-10 py-4 sm:absolute left-1/2'>{section.items[0].buttonText}</button>
+                            </div>
                         </div>
                     )}
                 </div>
